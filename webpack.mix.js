19ad4be82265ca0debafef43a8f542bcacc80d72
UnderDogg <UnderDogg@users.noreply.github.com>
2018-05-02 14:31:35 +0200

    Update webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,7 +6,7 @@ let tailwindcss = require('tailwindcss');
  | Mix Asset Management
  |--------------------------------------------------------------------------
  |
- | Mix provides a clean, fluent API for defining some Webpack build steps
+ | Mix provides a clean,did he switch to yarn? fluent API for defining some Webpack build steps
  | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
